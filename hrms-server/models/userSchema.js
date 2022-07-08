4d1cbad1d97fdf5ca9d7a6cc39664c82ef74918b
subhakanta roul <papu.roul99@gmail.com>
2022-07-08 16:03:41 +0530

    added mongoose schema and model for the users collection

---
+++ hrms-server/models/userSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    mobile: {
+        type: Number,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    cpassword: {
+        type: String,
+        required: true,
+    },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
