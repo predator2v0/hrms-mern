0667c26fb7bc95439c0f2ee05570a027f0d6692f
subhakanta roul <papu.roul99@gmail.com>
2022-07-13 11:36:05 +0530

    optimized user login and register funciton and implemented password hashing

--- hrms-server/routes/auth.js
+++ hrms-server/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router();
-const user = require("../models/userSchema");
-
+const authRouter = express.Router();
+const user = require("../models/user");
+const bcrypt = require("bcryptjs");
 // register user router
-router.post("/register", async (req, res) => {
+authRouter.post("/register", async (req, res) => {
     try {
         const { name, email, mobile, password, cpassword } = req.body;
         if (!name || !email || !mobile || !password || !cpassword) {
@@ -11,15 +11,27 @@ router.post("/register", async (req, res) => {
         }
         const userAlreadyRegistered = await user.findOne({ email: email });
         if (userAlreadyRegistered) {
-            console.log("user already registered");
             return res.json({ msg: "user already registered" });
-        }
-        const newUser = new user({ name, email, mobile, password, cpassword });
-        const userCreated = await user.create(newUser);
-        if (userCreated) {
-            res.status(200).json({ msg: "user registered successfully." });
+        } else if (password !== cpassword) {
+            return res
+                .status(400)
+                .json({ msg: "password does not match confirm password" });
         } else {
-            res.status(400).json({ msg: "unable to register user." });
+            const newUser = new user({
+                name,
+                email,
+                mobile,
+                password,
+                cpassword,
+            });
+            newUser.password = await bcrypt.hash(newUser.password, 12);
+            newUser.cpassword = await bcrypt.hash(newUser.cpassword, 12);
+            const userCreated = await user.create(newUser);
+            if (userCreated) {
+                res.status(200).json({ msg: "user registered successfully." });
+            } else {
+                res.status(400).json({ msg: "unable to register user." });
+            }
         }
     } catch (err) {
         console.log(err);
@@ -28,26 +40,23 @@ router.post("/register", async (req, res) => {
 });
 
 // login router
-router.post("/login", async (req, res) => {
-    try{
+authRouter.post("/login", async (req, res) => {
+    try {
         const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(400).json({ msg: "fill all the fields" });
-    }
-    const userExists = await user.findOne({ email: email });
-    if (
-        userExists &&
-        userExists.email === email &&
-        userExists.password === password
-    ) {
-        res.status(200).json({ msg: "user login successful" });
-    } else {
-        res.status(400).json({ msg: "invalid credentials" });
-    }
-    } catch(err){
+        if (!email || !password) {
+            res.status(400).json({ msg: "fill all the fields" });
+        }
+        const userExists = await user.findOne({ email: email });
+        const matchPass = await bcrypt.compare(password, userExists.password);
+        if (userExists && userExists.email === email && matchPass) {
+            res.status(200).json({ msg: "user login successful" });
+        } else {
+            res.status(400).json({ msg: "invalid credentials" });
+        }
+    } catch (err) {
         console.log(err);
-        res.status(400).json({msg: 'unable to login. some error occurred'})
+        res.status(400).json({ msg: "unable to login. some error occurred" });
     }
 });
 
-module.exports = router;
+module.exports = authRouter;
