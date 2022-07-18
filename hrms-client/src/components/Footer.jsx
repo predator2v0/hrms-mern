ba8917f4a8d274ec8c2f804da75e909e78e41d0b
subhakanta roul <papu.roul99@gmail.com>
2022-07-18 19:28:48 +0530

    fed minor changes

--- hrms-client/src/components/Footer.jsx
+++ hrms-client/src/components/Footer.jsx
@@ -5,21 +5,22 @@ const Footer = () => {
         <div className='footer'>
             <div className='footer-container container'>
                 <div className='contact-info'>
-                    <div className="email-info">
+                    <div className='email-info'>
                         <i class='bi bi-envelope'></i>{" "}
                         <p>info.customcastle@gmail.com</p>
                     </div>
-                    <div className="address-info">
+                    <div className='address-info'>
                         <i class='bi bi-geo-alt'></i>
-                        <p>13A, 13C, tech park (19th floor), <br/> Dhenkanal, dhenkanal, Odisha, India <br /> PIN: 759014</p>
-                        
+                        <p>
+                            13A, 13C, tech park (19th floor), <br /> Dhenkanal,
+                            dhenkanal, Odisha, India <br /> PIN: 759014
+                        </p>
                     </div>
-                    <div className="social-icons">
+                    <div className='social-icons'>
                         <i class='bi bi-twitter'></i>
                         <i class='bi bi-facebook'></i>
                         <i class='bi bi-instagram'></i>
                         <i class='bi bi-youtube'></i>
-                        
                     </div>
                 </div>
                 <div className='quick-links'>
@@ -32,12 +33,32 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className='contact-form'>
-                    <form action="/contactus">
-                        <div className="form-group">
-                            <input type="text" className="form-control" id="name" placeholder="name"/>
-                            <input type="text" className="form-control" id="email" placeholder="email"/>
-                            <textarea className="form-control" id="email" placeholder="message" ></textarea>
-                            <input type="submit" className="btn submit-btn" />
+                    <form action='/contactus'>
+                        <div className='form-group'>
+                            <input
+                                type='text'
+                                className='form-control'
+                                id='name'
+                                placeholder='name'
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <input
+                                type='text'
+                                className='form-control'
+                                id='email'
+                                placeholder='email'
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <textarea
+                                className='form-control'
+                                id='email'
+                                placeholder='message'
+                            ></textarea>
+                        </div>
+                        <div className='form-group'>
+                            <input type='submit' className='btn submit-btn' />
                         </div>
                     </form>
                 </div>
